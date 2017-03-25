ca87f85de3252f0805bc9ee5903477d468495fbb
Thad Roe <git@thadroe.com>
2017-03-25 15:54:28 -0500

    Add susy

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,20 @@ var sourcemaps = require('gulp-sourcemaps');
 gulp.task('sass', function () {
   gulp.src('source/scss/main.scss')
     .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(sass({
+            outputStyle: 'expanded',
+            includePaths: ['node_modules/susy/sass']
+        }).on('error', sass.logError))
+        .pipe(autoprefixer(
+            'last 2 version',
+            'safari 5',
+            'ie 7',
+            'ie 8',
+            'ie 9',
+            'opera 12.1',
+            'ios 6',
+            'android 4'
+        ))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('assets/css/'));
 });
@@ -31,7 +43,12 @@ gulp.task('watch', function(){
 
     gulp.watch('source/scss/*.scss', ['sass']);
     gulp.watch('source/js/*.js', ['uglify']);
-    gulp.watch(['assets/css/main.css', '*.php', 'assets/js/*.js', 'template-parts/*.php'], function (files){
+    gulp.watch([
+        'assets/css/main.css',
+        '*.php',
+        'assets/js/*.js',
+        'template-parts/*.php'
+    ], function (files){
         livereload.changed(files)
     });
 });
