4b20c1a8cc056be739dd12eb8999cfe61db27d3a
Thad Roe <git@thadroe.com>
2017-03-26 00:01:44 -0500

    Content partials

--- index.php
+++ index.php
@@ -6,16 +6,38 @@ get_header(); ?>
 
 <main class="main-content-area">
 
-	<section class="primary-section">
-		<header class="primary-section-header">
+    <section class="primary-section">
 
-		</header>
-		<article class="article-container">
-			<header class="article-header">
-				<h1 class="article-title"></h1>
-			</header>
-			<p class="article-content"></p>
-		</article>
+        <header class="primary-section-header">
+
+        </header>
+
+	    <?php
+	    if ( have_posts() ) :
+
+		    if ( is_home() && ! is_front_page() ) : ?>
+                <header>
+                    <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+                </header>
+
+			    <?php
+		    endif;
+
+		    /* Start the Loop */
+		    while ( have_posts() ) : the_post();
+
+			    get_template_part( 'template-parts/content', get_post_format() );
+
+		    endwhile;
+
+		    the_posts_navigation();
+
+	    else :
+
+		    get_template_part( 'template-parts/content', 'none' );
+
+	    endif; ?>
+
 	</section>
 
 </main>
--- template-parts/content.php
+++ template-parts/content.php
@@ -2,3 +2,43 @@
 /**
  * Content partial
  */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('article-container'); ?>>
+
+    <header class="article-header">
+		<?php
+		if ( is_single() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
+
+		if ( 'post' === get_post_type() ) : ?>
+            <div class="entry-meta">
+				<?php thinthreadwp_posted_on(); ?>
+            </div>
+			<?php
+		endif; ?>
+    </header>
+
+    <div class="article-content">
+		<?php
+		the_content( sprintf(
+		/* translators: %s: Name of current post. */
+			wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'thinthreadwp' ), array( 'span' => array( 'class' => array() ) ) ),
+			the_title( '<span class="screen-reader-text">"', '"</span>', false )
+		) );
+
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'thinthreadwp' ),
+			'after'  => '</div>',
+		) );
+		?>
+    </div>
+
+    <footer class="article-footer">
+		<?php thinthreadwp_entry_footer(); ?>
+    </footer>
+
+</article>
